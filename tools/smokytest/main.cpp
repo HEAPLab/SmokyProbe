11341911f31c8b29ffe8af2e0d53a2e92e17f93c
Giuseppe Massari <joe.massanga@gmail.com>
2019-10-01 16:52:52 +0200

    [Tools] smokytest: dirty refactoring for calibration session
    Print averaged voltage values.
    
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

--- tools/smokytest/main.cpp
+++ tools/smokytest/main.cpp
@@ -5,6 +5,7 @@
 #include <getopt.h>
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include <chrono>
 #include <thread>
@@ -13,23 +14,81 @@
 #include "probe_uart_safe.h"
 
 
-void print_help(char * execname) {
-
+void print_help(char * execname)
+{
 	std::cout << "Usage: # " << execname << std::endl
-		  << "    [-d] /path/to/serial/device " << std::endl
-		  << "    [-n] nr. samplings " << std::endl
-		  << "    [-t] running time for energy computation " << std::endl;
+	          << "    [-d] /path/to/serial/device " << std::endl
+	          << "    [-n] nr. samplings " << std::endl
+	          << "    [-t] running time for energy computation "
+	          << "    [-c] max channel id number " << std::endl;
+
 }
 
+
+void print_insta_voltage(smokyprobe::Probe_UART & probe, int max_channel_id)
+{
+	// Instantaneous measures
+//	std::cout << "Print voltage values from 0 to "
+//	                << max_channel_id << std::endl;
+	for (int id = 0; id < max_channel_id; ++id) {
+		if (id > 0) std::cout << ",";
+		std::cout << probe.get_voltage_V(id);
+//		std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
+	std::cout << std::endl;
+}
+
+void print_avg_voltage(
+        smokyprobe::Probe_UART & probe,
+        unsigned int max_channel_id,
+        unsigned int nr_samples)
+{
+	std::vector<float> avg_values(max_channel_id + 1);
+	for (auto & ch_value : avg_values)
+		ch_value = 0.0;
+
+	for (unsigned int i = 0; i < nr_samples; ++i) {
+		for (int id = 0; id <= max_channel_id; ++id) {
+			avg_values[id] += probe.get_voltage_V(id) / nr_samples;
+		}
+	}
+
+	for (int id = 0; id <= max_channel_id; ++id) {
+		if (id > 0) std::cout << ",";
+		std::cout << avg_values[id];
+	}
+	std::cout << std::endl;
+}
+
+
+void print_energy(
+        smokyprobe::Probe_UART & probe,
+        unsigned int channel_id,
+        unsigned int nr_samples,
+        unsigned int running_time_s)
+{
+	// Energy samples
+	std::cout << std::endl << "=== Getting "
+	          << nr_samples << " energy samples for "
+	          << running_time_s << "s execution..." <<  std::endl;
+
+	for (int i = 0; i < nr_samples; ++i) {
+		probe.start_energy_sampling(channel_id);
+		std::this_thread::sleep_for(std::chrono::seconds(running_time_s));
+		float value = probe.stop_energy_sampling(channel_id);
+		std::cout << "energy: " << value << " J" << std::endl;
+	}
+}
 
 int main(int argc, char **argv)
 {
 	int opt;
 	char file_tty[20];
-	unsigned int count = 10;
+	unsigned int nr_samples = 10;
 	unsigned int running_time_s = 5;
+	int up_to_channel = -1;
 
-	while ((opt = getopt(argc, argv, "hd:n:t:")) != -1) {
+	while ((opt = getopt(argc, argv, "hd:n:t:c:")) != -1) {
 		switch (opt) {
 		case 'h':
 			print_help(argv[0]);
@@ -38,10 +97,13 @@ int main(int argc, char **argv)
 			strncpy(file_tty, optarg, 20);
 			break;
 		case 'n':
-			count = atoi(optarg);
+			nr_samples = atoi(optarg);
 			break;
 		case 't':
 			running_time_s = atoi(optarg);
+			break;
+		case 'c':
+			up_to_channel = atoi(optarg);
 			break;
 		default:
 			print_help(argv[0]);
@@ -56,35 +118,27 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	// Channel availability
-	for (int j = 0; j < probe.get_num_channels_max(); ++j) {
-		auto ret = probe.check_channel(j);
-		if (ret == smokyprobe::DeviceStatus::OK)
-			std::cout << "Channel: " << j << " = AVAILABLE" << std::endl;
-		else
-			std::cout << "Channel: " << j << " = NOT AVAILABLE " << (int) ret << std::endl;
+	if (up_to_channel < 0) {
+		up_to_channel = probe.get_num_channels_max() - 1;
+//		std::cout << "# Scanning up to channel: "
+//		          << up_to_channel << std::endl;
 	}
+	/*
+		// Channel availability
+		for (unsigned int id = 0; id < up_to_channel; ++id) {
+			auto ret = probe.check_channel(id);
+			if (ret == smokyprobe::DeviceStatus::OK) {
+				std::cout << "Channel: " << id << " = AVAILABLE" << std::endl;
+	//			std::cout << "info: " << probe.get_info(id) << std::endl;
+			} else {
+				std::cout << "Channel: " << id << " = NOT AVAILABLE "
+				          << (int) ret << std::endl;
+			}
 
-	// Device info
-	std::this_thread::sleep_for(std::chrono::seconds(2));
-	std::cout << "info: "<< probe.get_info(0) << std::endl;
-
-	// Instantaneous measures
-	std::cout << std::endl << "=== Getting " << count << " samples of measures..." << std::endl;
-	for (int i = 0; i < count; ++i) {
-		std::cout << "all: "<< probe.get_all(0) << std::endl;
-		std::this_thread::sleep_for(std::chrono::seconds(1));
-	}
-
-	// Enery samples
-	std::cout << std::endl << "=== Getting " << count << " energy samples for " 
-		  << running_time_s << "s execution..." <<  std::endl;
-	for (int i = 0; i < count; ++i) {
-		probe.start_energy_sampling(0);
-		std::this_thread::sleep_for(std::chrono::seconds(running_time_s));
-		float value = probe.stop_energy_sampling(0);
-		std::cout << "energy: " << value << " J" << std::endl;
-	}
+	//		std::this_thread::sleep_for(std::chrono::seconds(2));
+		}
+	*/
+	print_avg_voltage(probe, up_to_channel, nr_samples);
 
 	return 0;
 }
