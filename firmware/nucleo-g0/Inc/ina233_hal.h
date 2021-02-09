fd47ac109945f519b650886c866d7c902b0e4b98
Giuseppe Massari <joe.massanga@gmail.com>
2021-02-09 11:49:38 +0100

    [Firmware] Function definitions in ina233_hal.h header
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

--- firmware/nucleo-g0/Inc/ina233_hal.h
+++ firmware/nucleo-g0/Inc/ina233_hal.h
@@ -109,6 +109,22 @@ typedef enum {
 } INA233_FaultType;
 
 
+HAL_StatusTypeDef INA233_Init(I2C_HandleTypeDef * i2c_handle, uint16_t slave_addr);
+HAL_StatusTypeDef INA233_GetDeviceInfo(I2C_HandleTypeDef * i2c_handle, uint16_t slave_addr,	INA233_DeviceInfo * info);
+HAL_StatusTypeDef INA233_InitThresholds(I2C_HandleTypeDef * i2c_handle, uint16_t slave_addr);
+HAL_StatusTypeDef INA233_StatusCheck(I2C_HandleTypeDef * i2c_handle, uint16_t slave_addr, INA233_FaultType * fault);
+HAL_StatusTypeDef INA233_ClearFaults(I2C_HandleTypeDef * i2c_handle, uint16_t slave_addr);
+float INA233_To_RealValue(int16_t reg_value, float m, int16_t R, uint16_t b);
+HAL_StatusTypeDef INA233_ReadInputCurrent(I2C_HandleTypeDef * i2c_handle, uint16_t slave_addr, float * value);
+HAL_StatusTypeDef INA233_ReadInputVoltage(I2C_HandleTypeDef * i2c_handle, uint16_t slave_addr, float * value);
+HAL_StatusTypeDef INA233_ReadInputPower(I2C_HandleTypeDef * i2c_handle, uint16_t slave_addr, float * value);
+HAL_StatusTypeDef INA233_ReadShuntVoltage(I2C_HandleTypeDef * i2c_handle, uint16_t slave_addr, float * value);
+HAL_StatusTypeDef INA233_ReadInputPowerSamples(I2C_HandleTypeDef * i2c_handle, uint16_t slave_addr, INA233_PowerSampling * power_sampling);
+HAL_StatusTypeDef INA233_StartEnergySampling(I2C_HandleTypeDef * i2c_handle, uint16_t slave_addr, TIM_HandleTypeDef * htim);
+HAL_StatusTypeDef INA233_StopEnergySampling(I2C_HandleTypeDef * i2c_handle, uint16_t slave_addr, TIM_HandleTypeDef * htim, float * energy);
+HAL_StatusTypeDef INA233_ClearInputEnergy(I2C_HandleTypeDef * i2c_handle, uint16_t slave_addr);
+
+
 #ifdef __cplusplus
  }
 #endif
